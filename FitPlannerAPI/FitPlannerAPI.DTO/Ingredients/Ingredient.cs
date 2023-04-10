493c863a39f9d7ff4b131caf417187614474702e
andreeav19 <andra_368@yahoo.com>
2023-04-10 11:45:19 +0300

    defined ingredient profile and dto

--- FitPlannerAPI/FitPlannerAPI.DTO/Ingredients/Ingredient.cs
+++ FitPlannerAPI/FitPlannerAPI.DTO/Ingredients/Ingredient.cs
@@ -1,12 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using FitPlannerAPI.DTO.Base;
 
 namespace FitPlannerAPI.DTO.Ingredients
 {
-    internal class Ingredient
+    public class Ingredient : BaseDTO
     {
+        public string Name { get; set; }
+        public int Calories { get; set; }
+        public float Price { get; set; }
+        public int Quantity { get; set; }
     }
 }
--- FitPlannerAPI/FitPlannerAPI/Profiles/IngredientProfile.cs
+++ FitPlannerAPI/FitPlannerAPI/Profiles/IngredientProfile.cs
@@ -1,6 +1,12 @@
-﻿namespace FitPlannerAPI.Profiles
+﻿using AutoMapper;
+
+namespace FitPlannerAPI.Profiles
 {
-    public class IngredientProfile
+    public class IngredientProfile : Profile
     {
+        public IngredientProfile()
+        {
+            CreateMap<FitPlannerAPI.Models.Models.Ingredient, FitPlannerAPI.DTO.Ingredients.Ingredient>().ReverseMap();
+        }
     }
 }
