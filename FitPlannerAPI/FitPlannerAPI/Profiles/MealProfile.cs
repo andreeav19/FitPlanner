1eeafc9ffe48242f762b86cd6a585d5b635f230f
andreeav19 <andra_368@yahoo.com>
2023-04-11 03:50:37 +0300

    added automapper configuration for meal

---
+++ FitPlannerAPI/FitPlannerAPI/Profiles/MealProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+
+namespace FitPlannerAPI.Profiles
+{
+    public class MealProfile : Profile
+    {
+        public MealProfile()
+        {
+            CreateMap<FitPlannerAPI.Models.Models.Meal, FitPlannerAPI.DTO.Meals.Meal>().ReverseMap();
+            CreateMap<FitPlannerAPI.Models.Models.MealIngredient, FitPlannerAPI.DTO.Workouts.WorkoutExercisePost>().ReverseMap();
+        }
+    }
+}
