4853592ae6179840b35cf768d5fb65479983ce4e
andreeav19 <andra_368@yahoo.com>
2023-04-11 15:12:22 +0300

    added user repository methods

--- FitPlannerAPI/FitPlannerAPI.Repositories/Repositories/UserRepositoriy/UserRepository.cs
+++ FitPlannerAPI/FitPlannerAPI.Repositories/Repositories/UserRepositoriy/UserRepository.cs
@@ -1,12 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using FitPlannerApi.Models;
+using FitPlannerAPI.Models.Models;
+using FitPlannerAPI.Repositories.Base;
 
 namespace FitPlannerAPI.Repositories.Repositories.UserRepositoriy
 {
-    internal class UserRepository
+    public class UserRepository : BaseRepository<User>, IUserRepository
     {
+        public UserRepository(FitPlannerDbContext context) : base(context)
+        {
+        }
+
+        public Task<bool> CreateUserMeal(UserMeal userMeal)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> CreateUserWorkout(UserWorkout userWorkout)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<UserMeal>> GetMealByUserId(Guid userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<UserWorkout>> GetWorkoutByUserId(Guid userId)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
